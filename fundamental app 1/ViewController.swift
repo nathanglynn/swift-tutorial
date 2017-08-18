4cceb63ae64693f334c63841306836cac4c29b51
Nathan Glynn <nateglynn@nathans-macbook-pro-home.local>
2017-08-18 13:00:40 -0700

    Initial Commit

---
+++ fundamental app 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fundamental app 1
+//
+//  Created by Nathan Glynn on 8/18/17.
+//  Copyright © 2017 Nathan Glynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
