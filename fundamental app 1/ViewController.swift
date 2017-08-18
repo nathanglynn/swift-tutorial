9cb804716f70353238652fb2e83f1d77ab087d08
Nathan Glynn <nathanglynn@gmail.com>
2017-08-18 16:34:00 -0700

    added buttons to select addition or substraction mode

--- fundamental app 1/ViewController.swift
+++ fundamental app 1/ViewController.swift
@@ -13,16 +13,26 @@ class ViewController: UIViewController {
     // connects the label added to the view to a variable
     @IBOutlet weak var theLabel: UILabel!
     
+    var addition = false
+    
+    @IBOutlet weak var modeIndicator: UILabel!
     
     @IBAction func buttonTap(_ sender: AnyObject) {
-        
-        var addition = false
         
         if addition {
             theLabel.text = "Sum = \(Double(text1.text!)! + Double(text2.text!)!)"} else {
             theLabel.text = "Minus = \(Double(text1.text!)! - Double(text2.text!)!)"}
     }
     
+    @IBAction func modeSelect(_ sender: Any) {
+        addition = true
+        modeIndicator.text = "+"
+    }
+    
+    @IBAction func subModeSelect(_ sender: Any) {
+        addition = false
+        modeIndicator.text = "-"
+    }
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
