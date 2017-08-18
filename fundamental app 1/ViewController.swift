4bf916e44f310a675def9ea8cc6ec230a9ff2158
Nathan Glynn <nathanglynn@gmail.com>
2017-08-18 16:19:19 -0700

    added a boolean thing

--- fundamental app 1/ViewController.swift
+++ fundamental app 1/ViewController.swift
@@ -9,36 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     // connects the label added to the view to a variable
     @IBOutlet weak var theLabel: UILabel!
-
+    
     
     @IBAction func buttonTap(_ sender: AnyObject) {
-        print(text1.text!)
-        print(text2.text!)
-    theLabel.text = "Sum = \(Double(text1.text!)! + Double(text2.text!)!)"
+        
+        var addition = false
+        
+        if addition {
+            theLabel.text = "Sum = \(Double(text1.text!)! + Double(text2.text!)!)"} else {
+            theLabel.text = "Minus = \(Double(text1.text!)! - Double(text2.text!)!)"}
     }
-
+    
     @IBOutlet weak var text1: UITextField!
     
     @IBOutlet weak var text2: UITextField!
     
     
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
