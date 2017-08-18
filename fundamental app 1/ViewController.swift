d71afcabafa9e076906524d8dd0be07460ae93c9
Nathan Glynn <nathanglynn@gmail.com>
2017-08-18 14:55:44 -0700

    first commit for a tutorial project

--- fundamental app 1/ViewController.swift
+++ fundamental app 1/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // connects the label added to the view to a variable
+    @IBOutlet weak var theLabel: UILabel!
+
+    var tapCount = 0
+    
+    // makes the first button set a new value for the label variable
+    @IBAction func buttonTap(_ sender: Any) {
+        theLabel.text = "Primary Alt Label";
+        tapCount = tapCount + 1
+        if tapCount >= 10 {theLabel.text = "Primary Alt label overide"}
+    }
+
+    // makes the second button set a new, different value for the label variable
+    @IBAction func secondButtonTap(_ sender: Any) {
+    theLabel.text = "Secondary Alt Label"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
